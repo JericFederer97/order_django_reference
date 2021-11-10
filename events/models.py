9d72e1d3c79921bcbb4d9853cf346b0c8e34c0ab
jericfederer97 <jose.enrico_bandong@co-graph.co.jp>
2021-11-10 14:10:18 +0900

    initial commit

---
+++ events/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Venue(models.Model):
+	name = models.CharField('アイテムコード', max_length=120)
+	address = models.CharField('アイテム名', max_length=300)
+	zip_code = models.CharField('Zip Code', max_length=15)
+	phone = models.CharField('Contact Phone', max_length=25, blank=True)
+	web = models.URLField('Website Address', blank=True)
+	email_address = models.EmailField('Email Address', blank=True)
+	# owner = models.IntegerField("Owner", blank=False, default=1)
+	# owner = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+	owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+
+	def __str__(self):
+		return self.name
+
+
+class MyClubUser(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	email = models.EmailField('User Email')
+
+	def __str__(self):
+		return self.first_name + ' ' + self.last_name
+
+
+class Event(models.Model):
+	name = models.CharField('Event Name', max_length=120)
+	event_date = models.DateTimeField('Event Date')
+	venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+	#venue = models.CharField(max_length=120)
+	manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+	description = models.TextField(blank=True)
+	attendees = models.ManyToManyField(MyClubUser, blank=True)
+
+	def __str__(self):
+		return self.name
+
+class Csv(models.Model):
+	file_name = models.FileField(upload_to='events')
+	uploaded = models.DateTimeField(auto_now_add=True)
+	activated = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f"File ID: {self.id}"
